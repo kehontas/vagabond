f694456e00602ad85a1952b2a13712b25aafa88a
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-20 12:11:11 -0800

    created relationship (added users to posts table)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120190830) do
+ActiveRecord::Schema.define(version: 20160120200816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160120190830) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -33,4 +36,5 @@ ActiveRecord::Schema.define(version: 20160120190830) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "posts", "users"
 end
