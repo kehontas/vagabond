c15ab204ebb969034bebebe3aea96280c814dd61
chan-d <chan.danny516@gmail.com>
2016-01-19 17:07:20 -0800

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   get "/users", to: "users#index", as: "users"
-
   get "/users/new", to: "users#new", as: "new_user"
-
   post "/users", to: "user#create"
-
   get "/users/:id", to: "users#show", as: "user"
+  
 end
