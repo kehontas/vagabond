6fcfd5a13e62ad483ff41f58e522a5595dc20960
kehontas <kehontas@gmail.com>
2016-01-19 16:50:11 -0800

    added post route for create and show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+  	user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
+  	@user = User.create(user_params)
+
+  	redirect_to root_path
+  end
+
+  def show
+  	@user = User.find_by_id(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get "/users", to: "users#index", as: "users"
 
   get "/users/new", to: "users#new", as: "new_user"
+
+  post "/users", to: "user#create"
+
+  get "/users/:id", to: "users#show", as: "user"
 end
