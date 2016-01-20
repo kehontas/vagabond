503c685295ca45b88c8f7ccfc728c44a1e78c796
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-20 11:57:39 -0800

    finished post index, new & create routes, views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def show
+  end
+
+  def create
+    post_params = params.require(:post).permit(:title, :content)
+    @post = Post.create(post_params)
+
+    redirect_to posts_path
   end
 end
