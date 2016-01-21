eb423b250901ab508c55ac259bbb33afae7d6301
tscurrie <tscurrie85@gmail.com>
2016-01-21 11:14:31 -0800

    updating branch to staging branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: "users#index"
+  get 'pages/home'
+
+  root '/' to: 'pages#home'
 
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
