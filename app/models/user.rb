a5d54f56e86a083829a962ac05b584dfe105b881
kehontas <kehontas@gmail.com>
2016-01-21 09:25:28 -0800

    profile on create no default yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
   has_many :posts
 
-   has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:medium/missing.jpeg"
+   has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/default.jpeg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
 
