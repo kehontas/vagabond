140b5f41b7503c2e64200e82f89d8637c5386a7d
kehontas <kehontas@gmail.com>
2016-01-21 07:39:57 -0800

    closing computer almost have default photo working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 	has_secure_password
   has_many :posts
 
-   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+   has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:medium/missing.jpeg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  
 
 	def self.confirm(params)
 		@user = User.find_by({email: params[:email]})
