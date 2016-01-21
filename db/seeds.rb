92faa779ac157d8e142f8fa6102eb04f2aaca5e7
chan-d <chan.danny516@gmail.com>
2016-01-21 13:57:38 -0800

    moved photo to city#show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,9 @@
  user = User.create({email: 't@t.com', first_name: 't', last_name: 't', current_city: 't', password: 't'})
  user2 = User.create({email: 't2@.com', first_name: 't2', last_name: 't2', current_city: 't2', password: 't2'})
  user3 = User.create({email: 't3@.com', first_name: 't3', last_name: 't3', current_city: 't3', password: 't'})
-<<<<<<< HEAD
- post = Post.create({title: 'my first post', content: "this is my first post yay!"})
- post2 = Post.create({title: 'my second post', content: "this is my second post yay!"})
- post3 = Post.create({title: 'my third post', content: "this is my third post yay!"})
 
- city1 = City.create({name: 'San Francisco'})
-=======
  post = Post.create({title: 'my first post', content: "this is my first post yay!", user_id: 1})
  post2 = Post.create({title: 'my second post', content: "this is my second post yay!", user_id: 1})
  post3 = Post.create({title: 'my third post', content: "this is my third post yay!", user_id: 2})
->>>>>>> 206f548cb65b66ce84fde8de479c0016c01d58af
+ city1 = City.create({name: 'San Francisco'})
+
