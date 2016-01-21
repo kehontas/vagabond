f365ef5b834811cf21f43a7698de21df408690b7
chan-d <chan.danny516@gmail.com>
2016-01-21 10:54:10 -0800

    splash page created, routed and controlled, css not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: "users#index"
+  
+  root to: "pages#home"
+
+  get '/', to: 'pages#home'
 
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
