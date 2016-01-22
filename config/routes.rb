a88f3707a6742649be96af34ee9b6b070ace9c56
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-21 19:59:36 -0800

    edit & create done: created route paths, controller methods & edit view page

--- config/routes.rb
+++ config/routes.rb
@@ -21,16 +21,9 @@ Rails.application.routes.draw do
   get "/posts/new", to: "posts#new", as: "new_post"
   post "/posts", to: "posts#create"
   get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
   delete "/posts/:id", to: "posts#destroy", as: "delete_post"
-
-
-
-
-
-
-
-
-
 
   get '/cities', to: 'cities#index'
   get '/cities/new', to: 'cities#new'
